57a002cb727645fa54ca7e45383b92a69db52f11
axelfleureau <axel@wearerighello.com>
2026-03-31 06:55:53 +0000

    recommit

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapterNode from '@sveltejs/adapter-node';
 import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const isVercel = process.env.VERCEL === '1'; 
+const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
