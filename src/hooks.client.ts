af3332091b3b3aa03cced5d3d64ad0b9f02795f3
axelfleureau <axel@wearerighello.com>
2026-04-20 12:43:31 +0000

    recommit

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {  
-  navigator.serviceWorker.register('/sw.js').catch(() => {});
+  navigator.serviceWorker.register('/sw.js').catch(() => {}); 
 }
 
 export const handleError = ({ error, event }: { error: unknown; event: unknown }) => { 
